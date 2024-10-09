cd88ea7a04aa93b3f28584e32a3c5faa4f1a6602
galya <galya@mishas-air.thehome>
2024-10-08 18:24:42 -0700

    something

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -18,25 +18,18 @@ public class RobotSquare {
 
 		// PEN. Put the robot's pen down so it can draw, Use this command:
 		/** rob.penDown(); **/
+		rob.penDown();
 
 		// SPEED. Make the robot move quickly. Use this command:
 		/** rob.setSpeed(<speed>); **/
+		rob.setSpeed(500);
 
 		// COUNT. Create an int variable that will count how many sides of the square we
 		// have drawn.
 		// The start value will be zero because no sides have yet been drawn. Use this
 		// code:
 		/** int count = 0; **/
-
-		// DRAW. Draw a line. Use this command:
-		/** rob.move(<distance>); **/
-
-		// TURN. Turn 90 degrees. Use this command:
-		/** rob.turn(<angle>); **/
-
-		// INCREASE COUNT. Add one to the number of sides that have now been drawn like
-		// this:
-		/** count += 1; **/
+		int count = 0;
 
 		// Run the program. Did the Robot draw a line and turn?
 
@@ -51,6 +44,12 @@ public class RobotSquare {
 		 * INCREASE COUNT
 		 * }
 		 **/
+		while(count < 4) {
+			rob.move(109);
+			rob.turn(90);
+			count ++;
+		}
+	
 
 		// Run the program again. Did your Robot draw a square?
 
--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -13,12 +13,20 @@ public class RobotSpiral {
 		
 		// Create a new Robot
 		
+		Robot r1 = new Robot();
+		
 		// Set your robot's pen down 
 		
+		r1.penDown();
+		
 		// SPEED. Set the robot to go at max speed (100)
+		
+		r1.setSpeed(500);
 
 		// COUNT. Create an int variable that will count how many lines of the spiral we have drawn.
 			//        Start its value as zero.
+		
+		int count = 0;
 
 		// LOOP. Start a while loop to repeat the COLOR, DRAW, TURN, and COUNT code below until 50 lines have been drawn
 
@@ -32,6 +40,14 @@ public class RobotSpiral {
 			// COUNT.   Increase the count of how many lines have been drawn so far ( count+=1 )
 
 			// Change the robot pen width to the current value of the count variable 
+		
+		while (count < 50) {
+			r1.setRandomPenColor();
+			r1.move(5 * count);
+			r1.turn(360/7);
+			count ++;
+			r1.setPenWidth(count);
+		}
 
 
 		//  End the while loop here
